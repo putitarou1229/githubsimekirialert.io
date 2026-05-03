830f7053f309b96cad6b1cd88e46d0cfb52eb103
putitarou1229 <putitarou1229@gmail.com>
2026-05-03 12:16:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("app").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/app.js"
-      ]);
-    })
-  );
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCPQiIjBJ_yQj5IVb54CZfEAh4CyJEg",
+  messagingSenderId: "747701425490",
+  projectId: "alert-55bd2",
+  appId: "1:747701425490:web:f77d3cc86c420567aad68b"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body
+  });
 });
