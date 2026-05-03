800f5f8ba0fa4d90e5bb843a083b3f883b8e976a
putitarou1229 <putitarou1229@gmail.com>
2026-05-03 19:25:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
@@ -10,8 +11,39 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
+// 🔔 通知（バックグラウンド）
 messaging.onBackgroundMessage(function(payload) {
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body
   });
+});
+
+
+// 📦 オフラインキャッシュ
+const CACHE_NAME = "todo-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+// インストール時キャッシュ
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// フェッチ制御（オフライン対応）
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
